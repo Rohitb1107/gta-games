dc9c1e2f1d1925dc941081482f9d8c72ef1773eb
Rohit <stylishrb711@gmail.com>
2022-04-01 13:14:44 +0530

    SeeAll Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AllCard from "./Components/Card/Card";
 import Product from "./Components/Product/Product";
 import { Routes, Route } from "react-router-dom";
+import SeeAll from "./Components/SeeAll/SeeAll";
 
 function App() {
   return (
     <>
-      {/* <AllCard /> */}
-      <Product />
+      <AllCard />
+      {/* <Product /> */}
+      {/* <SeeAll /> */}
 
       {/* <Routes>
         <Route exact path="/" component={AllCard} />
